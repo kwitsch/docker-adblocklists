51d2e54f3a0e48fc9dd779cb64aa0885400ab6f0
Kwitsch <kwitsch@gmail.com>
2021-10-02 05:56:27 +0200

    healthcheck fix 2

--- src/healthcheck/main.go
+++ src/healthcheck/main.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
 	"net/http"
 	"os"
 )
 
 func main() {
-	resp, err := http.Get("127.0.0.1:80/healtcheck")
-	if err == nil {
-		body, err := ioutil.ReadAll(resp.Body)
-		if err == nil {
-			if string(body) == "ok" {
-				os.Exit(0)
-				return
-			}
-		}
+	_, err := http.Get("http://127.0.0.1:80/healtcheck")
+	if err != nil {
+		fmt.Println("Healthchek fail")
+		os.Exit(1)
+	} else {
+		fmt.Println("Healthchek ok")
+		os.Exit(0)
 	}
-	os.Exit(1)
 }
